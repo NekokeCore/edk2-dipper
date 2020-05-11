120cae8a18c75c40d71d79649efeb0362a0bafb3
NekokeCore <nekokecore@gmail.com>
2020-05-11 21:14:07 +0800

    Update Some Files

--- firstrun.sh
+++ firstrun.sh
@@ -3,6 +3,8 @@
 # do this first:
 # https://github.com/tianocore/tianocore.github.io/wiki/Using-EDK-II-with-Native-GCC#Install_required_software_from_apt
 set -e
+figlet Edk2-Dipper
+echo [BuildTools] Now Building Edk2 Environment
 . build_common.sh
 make -C ../edk2/BaseTools
-
+echo [BuildTools] Done.
