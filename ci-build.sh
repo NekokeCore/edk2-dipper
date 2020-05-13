0a5744d82456220e849534cadd795e9a44b49396
NekokeCore <nekokecore@gmail.com>
2020-05-13 17:10:16 +0800

    Fix CI build

--- ci-build.sh
+++ ci-build.sh
@@ -9,5 +9,5 @@ cd ..
 git clone https://github.com/tianocore/edk2.git --recursive
 git clone https://github.com/tianocore/edk2-platforms.git
 cd "$curdir"
-./firstrun.sh
-./build.sh
+bash firstrun.sh
+bash build.sh
